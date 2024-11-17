575d5794fff1762be77ab461930c6245a7009164
jklee-github <jklee.apply@gmail.com>
2024-11-17 18:14:47 -0500

    dialog fuction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DialogComponent } from './dialog/dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DialogComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'simulate-dblclick';
+
+  constructor(private dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(DialogComponent);
+  }
 }
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
   imports: [],
   templateUrl: './dialog.component.html',
-  styleUrl: './dialog.component.scss'
+  styleUrl: './dialog.component.scss',
 })
 export class DialogComponent {
+  @ViewChild('proceedButton', { static: true }) proceedButton!: ElementRef;
 
+  // Handle the double-click event
+  onProceed() {
+    console.log('Double-click detected on "Yes, Proceed" button!');
+  }
+
+  // Simulate a double-click event
+  simulateDoubleClick() {
+    const dblClickEvent = new MouseEvent('dblclick', {
+      bubbles: true,
+      cancelable: true,
+    });
+    this.proceedButton.nativeElement.dispatchEvent(dblClickEvent);
+  }
 }
